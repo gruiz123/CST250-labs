2bf800a265e5b8e768e1768942c74aedb1e0471a
gruiz123 <78010168+gruiz123@users.noreply.github.com>
2021-03-03 02:06:40 -0600

    lab06

---
+++ lab06/src/main/java/edu/ru/lab06/Rectangle.java
@@ -0,0 +1,21 @@
+package edu.ru.lab06;
+
+public class Rectangle {
+    
+        int width;
+        int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public void draw(){
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < width; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
